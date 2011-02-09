9577f0d878e6cb4a1244db70711380a94819b03c
XhmikosR <xhmikosr@users.sourceforge.net>
2011-02-09 20:12:46 +0000

    cosmetics in the build.bat
    git-svn-id: https://perfmonbar.googlecode.com/svn/trunk@11 4b0796f0-2a0a-b979-5118-a1cf9c0de4af

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 TITLE Compiling PerfmonBar...
 
-IF NOT DEFINED VS100COMNTOOLS ECHO:Visual Studio 2010 wasn't found&&GOTO :EndWithError
+IF NOT DEFINED VS100COMNTOOLS ECHO Visual Studio 2010 wasn't found & GOTO EndWithError
 
 CD /D %~dp0
 
@@ -10,23 +10,24 @@ CALL "%VS100COMNTOOLS%vsvars32.bat" >NUL
 
 TITLE Compiling PerfmonBar - Release^|Win32...
 devenv PerfmonBar.sln /Rebuild "Release|Win32"
-IF %ERRORLEVEL% NEQ 0 GOTO :EndWithError
+IF %ERRORLEVEL% NEQ 0 GOTO EndWithError
 
 TITLE Compiling PerfmonBar - Release^|x64...
 devenv PerfmonBar.sln /Rebuild "Release|x64"
-IF %ERRORLEVEL% NEQ 0 GOTO :EndWithError
+IF %ERRORLEVEL% NEQ 0 GOTO EndWithError
 ECHO.
 
 
 :END
 ECHO.
-ENDLOCAL && PAUSE
+ENDLOCAL
+PAUSE
 EXIT /B
 
 :EndWithError
 Title Compiling PerfmonBar [ERROR]
-ECHO. && ECHO.
-ECHO: **ERROR: Build failed and aborted!**
+ECHO. & ECHO.
+ECHO **ERROR: Build failed and aborted!**
 PAUSE
 ENDLOCAL
 EXIT
