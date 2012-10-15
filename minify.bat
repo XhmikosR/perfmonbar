289b2d2b5d38b915936a5181f11754d606b63d26
XhmikosR <xhmikosr@users.sourceforge.net>
2012-10-15 16:30:50 +0300

    use clean-css 0.7.0

--- minify.bat
+++ minify.bat
@@ -5,7 +5,7 @@ SET PATH=C:\MSYS\bin;%PATH%
 
 ECHO Minifying and combining css and js files...
 
-cat css\stylesheet.css css\jquery.fancybox.css | cleancss -o css\pack.css && ^
+cat css\stylesheet.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css && ^
 uglifyjs --no-copyright -o js\jquery.fancybox.min.js js\jquery.fancybox.js
 
 ENDLOCAL
