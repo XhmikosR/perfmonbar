a02d611ff4835ce804e853cb0df8ecfcd6b607bc
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-03 11:05:41 +0200

    minify.bat: use type since we are on Windows

--- minify.bat
+++ minify.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 SETLOCAL
 
-SET PATH=C:\MSYS\bin;%PATH%
-
 ECHO Minifying and combining css and js files...
 
-cat css\stylesheet.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css && ^
+type css\stylesheet.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css && ^
 uglifyjs --no-copyright -o js\jquery.fancybox.min.js js\jquery.fancybox.js
 
 ENDLOCAL
