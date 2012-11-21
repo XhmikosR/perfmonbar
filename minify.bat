8a56d6339921461bd8c34174aedfd44f1150257f
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-21 18:09:58 +0200

    switch to uglifyjs 2.2.0

--- minify.bat
+++ minify.bat
@@ -4,7 +4,7 @@ SETLOCAL
 ECHO Minifying and combining css and js files...
 
 TYPE css\jquery.fancybox.css css\normalize.css css\stylesheet.css | cleancss --s0 -o css\pack.css
-uglifyjs --no-copyright -o js\jquery.fancybox.min.js js\jquery.fancybox.js
+cmd /c uglifyjs js\jquery.fancybox.js --compress --mangle -o js\jquery.fancybox.min.js
 
 ENDLOCAL
-PAUSE
+rem PAUSE
