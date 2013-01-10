65b538c31555d06ec25e07523956c138e0a6edb2
XhmikosR <xhmikosr@users.sourceforge.net>
2013-01-10 09:49:29 +0200

    update update_version.bat and version.sh from MPC-HC adopted for PerfmonBar

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# (C) 2012 see Authors.txt
+#!/bin/bash
+# (C) 2012-2013 see Authors.txt
 #
 # This file is part of MPC-HC.
 #
@@ -16,66 +16,67 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-[ -n "$1" ] && cd $1
 # This is the last svn changeset, the number and hash can be automatically
 # calculated, but it is slow to do that. So it is better to have it hardcoded.
-# We'll need to update this with the last svn data before committing this script
-SVNREV=55
-SVNHASH="688c3dd699fda7a256dca048466c1902b2c32d6e"
+svnrev=55
+svnhash="688c3dd699fda7a256dca048466c1902b2c32d6e"
 
-if [ ! -d ".git" ] || ! command -v git >/dev/null 2>&1 ; then
-  # If the folder ".git" doesn't exist or git isn't present then we use hardcoded values
-  HASH=0000000
-  VER=0
+versionfile="./src/Version.h"
+
+#If the git command isn't available or we are not inside a git repo use hardcoded values
+if ! git rev-parse --git-dir > /dev/null 2>&1; then
+  hash=0000000
+  ver=0
 else
   # Get the current branch name
-  BRANCH=`git branch | grep "^\*" | awk '{print $2}'`
+  branch=$(git symbolic-ref -q HEAD) && branch=${branch##refs/heads/} || branch="no branch"
   # If we are on the master branch
-  if [ "$BRANCH" == "master" ] ; then
-    BASE="HEAD"
+  if [[ "$branch" == "master" ]]; then
+    base="HEAD"
   # If we are on another branch that isn't master, we want extra info like on
   # which commit from master it is based on and what its hash is. This assumes we
   # won't ever branch from a changeset from before the move to git
   else
     # Get where the branch is based on master
-    BASE=`git merge-base master HEAD`
+    base=$(git merge-base master HEAD)
+    base_ver=$(git rev-list --count $svnhash..$base)
+    base_ver=$((base_ver+svnrev))
 
-    VERSION_INFO="#define BRANCH _T(\"$BRANCH\")\n"
-    VER_FULL=" ($BRANCH)"
+    version_info="#define BRANCH _T(\"$branch\")"$'\n'
+    ver_full=" ($branch) (master@${base_ver:0:7})"
   fi
 
   # Count how many changesets we have since the last svn changeset
-  VER=`git rev-list $SVNHASH..$BASE | wc -l`
+  ver=$(git rev-list --count $svnhash..HEAD)
   # Now add it with to last svn revision number
-  VER=$(($VER+$SVNREV))
+  ver=$((ver+svnrev))
 
   # Get the abbreviated hash of the current changeset
-  HASH=`git log -n1 --format=%h`
+  hash=$(git rev-parse --short HEAD)
 
 fi
 
-VER_FULL="_T(\"$VER ($HASH)$VER_FULL\")"
+ver_full="_T(\"$ver ($hash)$ver_full\")"
 
-VERSION_INFO+="#define VER_HASH \"$HASH\"\n"
-VERSION_INFO+="#define VER_NUM 1,0,2,$VER\n"
-VERSION_INFO+="#define VER_STR \"1.0.2.$VER\""
+version_info+="#define VER_HASH _T(\"$hash\")"$'\n'
+version_info+="#define VER_NUM 1,0,2,$ver"$'\n'
+version_info+="#define VER_STR \"1.0.2.$ver\""
 
-if [ "$BRANCH" ] ; then
-  echo -e "On branch: $BRANCH"
+if [[ "$branch" ]]; then
+  echo "On branch: $branch"
 fi
-echo -e "Hash:      $HASH"
-if [ "$BRANCH" ] && git status | grep -q "modified:" ; then
-  echo -e "Revision:  $VER (Local modifications found)"
+echo "Hash:      $hash"
+if [[ "$branch" ]] && ! git diff-index --quiet HEAD; then
+  echo "Revision:  $ver (Local modifications found)"
 else
-  echo -e "Revision:  $VER"
+  echo "Revision:  $ver"
+fi
+if [[ "$branch" ]] && [[ "$branch" != "master" ]]; then
+  echo "Mergebase: master@${base_ver} (${base:0:7})"
 fi
 
-if [ -f ./src/Version.h ] ; then
-  VERSION_INFO_OLD=`<./src/Version.h`
-fi
-
-# Only write the files if the version information has changed
-if [ "$(echo $VERSION_INFO | sed -e 's/\\n/ /g')" != "$(echo $VERSION_INFO_OLD)" ] ; then
+# Update Version.h if it does not exist, or if version information  was changed.
+if [[ ! -f "$versionfile" ]] || [[ "$version_info" != "$(<"$versionfile")" ]]; then
   # Write the version information to Version.h
-  echo -e $VERSION_INFO > ./src/Version.h
+  echo "$version_info" > "$versionfile"
 fi
