4c2db61b909036654ab50a6da4c13e06e05840ec
Chris Conti <cmconti@users.noreply.github.com>
2021-04-18 17:49:53 -0400

    check for powershell version and call ps1 script

--- update_version.bat
+++ update_version.bat
@@ -5,13 +5,11 @@ PUSHD %~dp0
 
 IF EXIST "build.user.bat" (CALL "build.user.bat")
 
-SET PATH=%MSYS%\bin;%PATH%
+rem check powershell installed and at least v5.1
+powershell -Command "if ([Version]'5.1' -gt $PSVersionTable.PSVersion){exit 1;}" > NUL 2>&1
+if ERRORLEVEL 1 GOTO :MissingPowershell
 
-FOR %%G IN (bash.exe) DO (SET FOUND=%%~$PATH:G)
-IF NOT DEFINED FOUND GOTO MissingVar
-
-bash.exe ./version.sh
-
+powershell -ExecutionPolicy Bypass -File .\version.ps1
 
 :END
 POPD
@@ -19,12 +17,10 @@ ENDLOCAL
 EXIT /B
 
 
-:MissingVar
+:MissingPowershell
 COLOR 0C
 TITLE ERROR
-ECHO MSYS (bash.exe) wasn't found. Create a file build.user.bat and set the variable there.
-ECHO.
-ECHO SET "MSYS=H:\progs\MSYS"
+ECHO Powershell 5.1 or later is not installed
 ECHO. & ECHO.
 ECHO Press any key to exit...
 PAUSE >NUL
